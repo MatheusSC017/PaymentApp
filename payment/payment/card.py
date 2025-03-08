565676e57145ae2de8c637ccf0046f991d0632cc
MatheusSC017 <matheussimao2101@gmail.com>
2025-03-08 15:55:37 -0300

    Cleaning code, putting the code rellated to card payment in a separeted class

---
+++ payment/payment/card.py
@@ -0,0 +1,80 @@
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import mercadopago
+import base64
+import os
+
+
+class Card:
+    PRIVATE_KEY = RSA.importKey(os.environ.get("PAYMENT_PRIVATE_KEY").replace('\\n', '\n'))
+    PRIVATE_CIPHER = PKCS1_OAEP.new(PRIVATE_KEY)
+
+    PUBLIC_KEY = RSA.importKey(os.environ.get("PAYMENT_PUBLIC_KEY").replace('\\n', '\n'))
+    PUBLIC_CIPHER = PKCS1_OAEP.new(PUBLIC_KEY)
+
+    MP_PRIVATE_KEY = os.environ.get("PAYMENT_MP_PRIVATE_KEY")
+    MP_PUBLIC_KEY = os.environ.get("PAYMENT_MP_PUBLIC_KEY")
+
+    def encrypt_data(self, purchase_identification):
+        cipher_purchase = self.PUBLIC_CIPHER.encrypt(purchase_identification.encode('utf-8'))
+        return base64.b64encode(cipher_purchase).decode('utf-8')
+
+    def decrypt_purchase_identification(self, encrypted_data):
+        try:
+            purchase_identification = self.PRIVATE_CIPHER.decrypt(base64.b64decode(encrypted_data.encode('utf-8'))).decode(
+                "utf8")
+            return purchase_identification.split(",")
+        except Exception as e:
+            raise ValueError(f"Failed to decrypt purchase identification: {e}")
+
+    @staticmethod
+    def build_purchase_identification(data):
+        ordered_fields = [
+            data['identificationType'],
+            data['identificationNumber'],
+            data['email'],
+            data['description'],
+            data['amount']
+        ]
+        return ",".join(ordered_fields)
+
+    def process_payment(self, data):
+        sdk = mercadopago.SDK(self.MP_PRIVATE_KEY)
+        request_options = mercadopago.config.RequestOptions()
+        request_options.custom_headers = {'x-idempotency-key': data["purchase_identification"]}
+
+        payment_data = {
+            "transaction_amount": float(data["transaction_amount"]),
+            "token": data["token"],
+            "description": data["description"],
+            "installments": int(data["installments"]),
+            "payment_method_id": data["payment_method_id"],
+            "payer": {
+                "email": data["payer"]["email"],
+                "identification": {
+                    "type": data["payer"]["identification"]["type"],
+                    "number": data["payer"]["identification"]["number"],
+                },
+            },
+        }
+
+        response = sdk.payment().create(payment_data, request_options)
+        return response["response"]
+
+    @staticmethod
+    def validate_purchase_data(decrypted_data, request_data):
+        expected_data = [
+            request_data["payer"]["identification"]["type"],
+            request_data["payer"]["identification"]["number"],
+            request_data["payer"]["email"],
+            request_data["description"],
+            str(request_data["transaction_amount"]),
+        ]
+        return decrypted_data == expected_data
+
+    @staticmethod
+    def validate_form_data(data):
+        return (all(data.values()) and
+                data['identificationType'] in ('CPF', 'CNPJ') and
+                data['amount'].replace('.', '', 1).isdigit())
+
--- payment/routes.py
+++ payment/routes.py
@@ -1,17 +1,10 @@
 from flask.blueprints import Blueprint
 from flask import render_template, request, jsonify
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.PublicKey import RSA
-import mercadopago
-import os
-import base64
+from payment.payment.card import Card
+
 
 payment_bp = Blueprint('payment', __name__, template_folder='templates')
-PRIVATE_KEY = RSA.importKey(os.environ.get("PAYMENT_PRIVATE_KEY").replace('\\n', '\n'))
-PRIVATE_CIPHER = PKCS1_OAEP.new(PRIVATE_KEY)
-PUBLIC_KEY = RSA.importKey(os.environ.get("PAYMENT_PUBLIC_KEY").replace('\\n', '\n'))
-PUBLIC_CIPHER = PKCS1_OAEP.new(PUBLIC_KEY)
-MP_PUBLIC_KEY = os.environ.get("PAYMENT_MP_PUBLIC_KEY")
+CARD_PAYMENT = Card()
 
 
 @payment_bp.route('/', methods=['POST', ])
@@ -19,39 +12,18 @@ def payment():
     form_data = {key: request.form.get(key, '').strip() for key in
                  ['amount', 'description', 'identificationType', 'identificationNumber', 'email']}
 
-    if not validate_form_data(form_data):
+    if not CARD_PAYMENT.validate_form_data(form_data):
         return jsonify({'error': 'Invalid parameters'}), 400
 
     try:
-        purchase_identification = build_purchase_identification(form_data)
-        cipher_purchase = base64.b64encode(encrypt_data(purchase_identification)).decode('utf-8')
+        purchase_identification = CARD_PAYMENT.build_purchase_identification(form_data)
+        cipher_purchase = CARD_PAYMENT.encrypt_data(purchase_identification)
 
         return render_template('index.html', **form_data,
-                               payment_mp_public_key=MP_PUBLIC_KEY,
+                               payment_mp_public_key=CARD_PAYMENT.MP_PUBLIC_KEY,
                                purchase_identification=cipher_purchase)
     except Exception as e:
         return jsonify({'error': 'Encryption failed', 'details': str(e)}), 500
-
-
-def validate_form_data(data):
-    return (all(data.values()) and
-            data['identificationType'] in ('CPF', 'CNPJ') and
-            data['amount'].replace('.', '', 1).isdigit())
-
-
-def build_purchase_identification(data):
-    ordered_fields = [
-        data['identificationType'],
-        data['identificationNumber'],
-        data['email'],
-        data['description'],
-        data['amount']
-    ]
-    return ",".join(ordered_fields)
-
-
-def encrypt_data(purchase_identification):
-    return PUBLIC_CIPHER.encrypt(purchase_identification.encode('utf-8'))
 
 
 @payment_bp.route('/process_payment', methods=['POST'])
@@ -62,13 +34,13 @@ def handle_payment():
             return jsonify({"error": "Invalid request format. Expected JSON"}), 400
 
         data = request.get_json()
-        purchase_identification = decrypt_purchase_identification(data["purchase_identification"])
+        purchase_identification = CARD_PAYMENT.decrypt_purchase_identification(data["purchase_identification"])
 
-        if not validate_purchase_data(purchase_identification, data):
+        if not CARD_PAYMENT.validate_purchase_data(purchase_identification, data):
             print("Invalid parameters")
             return jsonify({'error': 'Invalid parameters'}), 400
 
-        payment_response = process_payment(data)
+        payment_response = CARD_PAYMENT.process_payment(data)
         if payment_response.get('status') == "in_process":
             pass
 
@@ -80,49 +52,6 @@ def handle_payment():
     except Exception as e:
         print(f"Error processing payment: {e}")
         return jsonify({"error": "Internal server error"}), 500
-
-
-def decrypt_purchase_identification(encrypted_data):
-    try:
-        purchase_identification = PRIVATE_CIPHER.decrypt(base64.b64decode(encrypted_data.encode('utf-8'))).decode("utf8")
-        return purchase_identification.split(",")
-    except Exception as e:
-        raise ValueError(f"Failed to decrypt purchase identification: {e}")
-
-
-def validate_purchase_data(decrypted_data, request_data):
-    expected_data = [
-        request_data["payer"]["identification"]["type"],
-        request_data["payer"]["identification"]["number"],
-        request_data["payer"]["email"],
-        request_data["description"],
-        str(request_data["transaction_amount"]),
-    ]
-    return decrypted_data == expected_data
-
-
-def process_payment(data):
-    sdk = mercadopago.SDK(os.getenv("PAYMENT_MP_PRIVATE_KEY"))
-    request_options = mercadopago.config.RequestOptions()
-    request_options.custom_headers = {'x-idempotency-key': data["purchase_identification"]}
-
-    payment_data = {
-        "transaction_amount": float(data["transaction_amount"]),
-        "token": data["token"],
-        "description": data["description"],
-        "installments": int(data["installments"]),
-        "payment_method_id": data["payment_method_id"],
-        "payer": {
-            "email": data["payer"]["email"],
-            "identification": {
-                "type": data["payer"]["identification"]["type"],
-                "number": data["payer"]["identification"]["number"],
-            },
-        },
-    }
-
-    response = sdk.payment().create(payment_data, request_options)
-    return response["response"]
 
 
 @payment_bp.route('/payment_response', methods=['GET', ])
