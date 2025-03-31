1677163113955f30bc7b9fa33b2d7eb27e91cc4a
MatheusSC017 <matheussimao2101@gmail.com>
2025-03-31 08:55:46 -0300

    Including card class to manage card Crud

--- payment/clients/card.py
+++ payment/clients/card.py
@@ -6,37 +6,69 @@ class CardProxy:
     MP_SDK = mercadopago.SDK(os.environ.get("PAYMENT_MP_PRIVATE_KEY"))
 
     def add_card(self, customer_id, card_data):
-        card_response = self.MP_SDK.card().create(customer_id, card_data)
-        card = card_response["response"]
-        return card
+        response = self.MP_SDK.card().create(customer_id, card_data)
+        if response["status"] == 201:
+            return response["response"]
 
-    def update_card(self):
-        pass
+        print(response["status"])
+        print(response["response"])
+        return {}
 
-    def delete_card(self):
-        pass
+    def update_card(self, customer_id, card_id, card_data):
+        response = self.MP_SDK.card().update(customer_id, card_id, card_data)
+        if response["status"] == 200:
+            return response["response"]
 
-    def get_cards(self):
-        pass
+        print(response["status"])
+        print(response["response"])
+        return {}
 
-    def get_card(self):
-        pass
+    def delete_card(self, customer_id, card_id):
+        response = self.MP_SDK.card().delete(customer_id, card_id)
+        if response["status"] == 200:
+            return response["response"]
+
+        print(response["status"])
+        print(response["response"])
+        return {}
+
+    def get_card(self, customer_id, card_id):
+        response = self.MP_SDK.card().get(customer_id, card_id)
+        if response["status"] == 200:
+            return response["response"]
+
+        print(response["status"])
+        print(response["response"])
+        return {}
+
+    def get_cards(self, customer_id):
+        response = self.MP_SDK.card().list_all(customer_id)
+        if response["status"] == 200:
+            return response["response"]
+
+        print(response["status"])
+        print(response["response"])
+        return {}
 
 
 if __name__ == "__main__":
     card_proxy = CardProxy()
 
-    sdk = mercadopago.SDK(os.environ.get("PAYMENT_MP_PRIVATE_KEY"))
-
-    customer_data = {
-        "email": "test_payer_1234567@gmail.com"
-    }
-    customer_response = sdk.customer().create(customer_data)
-    customer = customer_response["response"]
-
     card_data = {
         "token": "af30c6e5a5bae0de91cd1296973e54f5"
     }
 
-    response = card_proxy.add_card(customer["id"], card_data)
+    card_response = card_proxy.add_card("2363817186-1wtI295gSu7H7y", card_data)
+    print(card_response)
+
+    response = card_proxy.get_cards("2363817186-1wtI295gSu7H7y")
+    print(response)
+
+    response = card_proxy.get_card("2363817186-1wtI295gSu7H7y", "1743418873544")
+    print(response)
+
+    response = card_proxy.update_card("2363817186-1wtI295gSu7H7y", "1743418873544", card_data)
+    print(response)
+
+    response = card_proxy.delete_card("2363817186-1wtI295gSu7H7y", "1743418873544")
     print(response)
